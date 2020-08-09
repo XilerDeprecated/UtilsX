14481be6b516a13d6bd8f5e696369c159fc45e73
Arthur De Witte <arthur.dewitte@gmail.com>
2020-08-09 21:38:05 +0200

    Added LICENSE and added base for setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+    name='utilsx',
+    packages=['utilsx'],
+    version='0.0.0',
+    license='MIT',
+    description='The public Xiler python utility library.',
+    author='Xiler Network - Arthurdw',
+    author_email='mail.arthurdw@gmail.com',
+    url='https://github.com/XilerNet/UtilsX',
+    download_url='none',
+    keywords=["Xiler", "utils"],
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 3 - Alpha',  # "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
