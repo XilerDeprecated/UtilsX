af551c580872582a03df60c5b5669a4891056a25
Arthur De Witte <arthur.dewitte@gmail.com>
2020-08-11 18:54:44 +0200

    version bump prep

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     author='Xiler Network - Arthurdw',
     author_email='mail.arthurdw@gmail.com',
     url='https://github.com/XilerNet/UtilsX',
-    download_url='https://github.com/XilerNet/UtilsX/archive/v0.1.2.tar.gz',
+    download_url='https://github.com/XilerNet/UtilsX/archive/v0.2.0.tar.gz',
     keywords=["Xiler", "Utils", "Discord", "Embed", "Formatting", "Console", "Pretty"],
     install_requires=[
         "discord.py"
